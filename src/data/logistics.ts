c7801c630f5f72e8284d8a1d29bec9d9015524bd
paep3nguin <paep3nguin@gmail.com>
2023-09-22 19:31:57 -0700

    Add Twilight logistic

--- src/data/logistics.ts
+++ src/data/logistics.ts
@@ -227,6 +227,31 @@ const logisticList: Array<Logistic> = [
     ],
   },
   {
+    name: 'Twilight',
+    rarity: Rarity.Orange,
+    maxAtk: 198,
+    levelL: 15,
+    levelM: 15,
+    levelR: 15,
+    modifiers: [
+      {
+        active: true,
+        name: 'Twilight 2-set',
+        description: 'Skill DMG +24%',
+        type: ModifierType.SkillDamage,
+        value: 24,
+      },
+      {
+        active: true,
+        name: 'Twilight 3-set',
+        description:
+          "Dealing DMG with active skill reduces target's resistance to that type of DMG -16% for 5s.",
+        type: ModifierType.ElementalResist,
+        value: 16,
+      },
+    ],
+  },
+  {
     name: 'Troubadour',
     rarity: Rarity.Purple,
     maxAtk: 148,
