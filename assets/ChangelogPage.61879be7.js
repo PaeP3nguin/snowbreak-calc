905c30dd7b85f3c41c159f9d3b0774c82264190a
paep3nguin <paep3nguin@gmail.com>
2024-03-24 01:42:52 -0700

    0a035ec

---
+++ assets/ChangelogPage.61879be7.js
@@ -0,0 +1,12 @@
+import{Q as e}from"./QPage.ca9ef033.js";import{d as n,_ as o,$ as r,a0 as s,a1 as t,a2 as d,aA as i}from"./index.a5e5c674.js";var c=`## 2024-03-07\r
+\r
+- Add setting to show/hide the stat boxes for the operative, weapon, and logistics. I figure most\r
+  people don't care about those details anyways\r
+\r
+## 2024-02-25\r
+\r
+- Alignment index can now be added as a modifier\r
+  - Katya M5 added as modifier\r
+- Fixed Katya weapon icon showing as pistol instead of crossbow\r
+- Added a changelog\r
+`;const _=n({__name:"ChangelogPage",setup(l){return(p,g)=>{const a=o("q-markdown");return r(),s(e,{class:"q-pa-lg",style:{"margin-top":"50px"}},{default:t(()=>[d(a,{src:i(c),"no-heading-anchor-links":""},null,8,["src"])]),_:1})}}});export{_ as default};
